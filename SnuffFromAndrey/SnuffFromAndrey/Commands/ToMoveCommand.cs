b981c1a185a5897d38466054fb36b2c50ea8875e
Aufniy Tiger <golenkotoxa@gmail.com>
2022-02-01 17:45:38 +0300

    start create view

---
+++ SnuffFromAndrey/SnuffFromAndrey/Commands/ToMoveCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnuffFromAndrey.Commands
+{
+    internal class ToMoveCommand
+    {
+    }
+}
---
+++ SnuffFromAndrey/SnuffFromAndrey/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace SnuffFromAndrey
+{
+    internal class ViewModel: INotifyPropertyChanged
+    {
+        public ICommand ToMove { get; set; }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
